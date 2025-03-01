9460afc1e5280ac67fd31814cdc037e450e03958
User <User@LAPTOP-EUEJQBO7>
2025-03-02 00:02:07 +0800

    Create a Concert.java class and Create attributes to define key properties of a concert object in the concert ticket booking system.

---
+++ ConcertTicket/src/main/java/com/mycompany/concertticket/Concert.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.concertticket;
+
+public class Concert {
+     private String Name;
+    private String Date;
+    private String Venue;
+    private double TicketPrice;
+    private String Performers;
+    private String Genre;
+    private String Duration;
+    private int AvailableTickets;
+
+}
---
+++ ConcertTicket/src/main/java/com/mycompany/concertticket/ConcertTicket.java
@@ -0,0 +1,8 @@
+package com.mycompany.concertticket;
+
+public class ConcertTicket {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
