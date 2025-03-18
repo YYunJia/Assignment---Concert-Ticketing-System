11a55d4e67b5b6057c9021b9873265e182732391
YunJia <yuen19900326@gmail.com>
2025-03-18 14:46:21 +0800

    Added news form field

--- NewsForm.aspx.cs
+++ NewsForm.aspx.cs
@@ -7,11 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace ConcertTicketing
 {
-    public partial class NewsForm : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+	public partial class NewsForm : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
 
-        }
-    }
+		}
+	}
 }
